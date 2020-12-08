742cf2ae0c195193a345f246c621a16f57b95a6d
土井康平 <cae35246@gmail.com>
2020-12-08 11:33:07 +0900

    4-13

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
+Route::group(['prefix' => 'admin' , 'middleware' => 'auth'], function() {
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
